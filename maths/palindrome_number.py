b4b3513892ee60650c1a890aecd86117900bc684
strongayush <ayushmishra901999@gmail.com>
2026-07-01 20:00:25 +0530

    Add palindrome number checker in palindrome_number.py
    Implemented a function to check if a number is a palindrome by converting it to a string and comparing it with its reverse.

---
+++ maths/palindrome_number.py
@@ -0,0 +1,18 @@
+"""
+Problem: Palindrome Number
+Link: https://leetcode.com/problems/palindrome-number/
+Difficulty: Easy
+Approach: Convert the int into str and then used the slice notation to check if it's Panlindrome
+Time: |Space: 
+"""
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        self.x = str(x)
+        if self.x[::1] == self.x[::-1]:
+            return True
+        else:
+            return False
